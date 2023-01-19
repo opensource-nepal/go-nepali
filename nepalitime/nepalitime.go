765ebef7432cda1757abdb29da3f2235e74be46b
Ajesh Sen Thapa <aj3sshh@gmail.com>
2023-01-19 21:45:15 +0545

    Now(), Date(), and Clock() added

--- nepalitime/nepalitime.go
+++ nepalitime/nepalitime.go
@@ -3,18 +3,35 @@ package nepalitime
 
 import (
 	"time"
+
+	"github.com/sugat009/nepali/dateConverter"
 )
 
 type NepaliTime struct {
 	// note that these members represent nepali values
-	year        int8
+	year        int16
 	month       int8
 	day         int8
-	hour        int8
-	minute      int8
-	second      int8
-	millisecond int8
-	nanosecond  int8
-
 	englishTime time.Time
+}
+
+// Now returns the current nepali time.
+func Now() NepaliTime {
+	englishTime := time.Now()
+	enYear, enMonth, enDay := englishTime.Date()
+	year, month, day := dateConverter.EnglishToNepali(enYear, int(enMonth), enDay)
+	return NepaliTime{int16(year), int8(month), int8(day), englishTime}
+}
+
+// Date returns the year, month, and day
+func (nt NepaliTime) Date() (year, month, day int) {
+	year = int(nt.year)
+	month = int(nt.month)
+	day = int(nt.day)
+	return
+}
+
+// Clock returns the hour, minute, and second of the day.
+func (nt NepaliTime) Clock() (hour, min, sec int) {
+	return nt.englishTime.Clock()
 }
