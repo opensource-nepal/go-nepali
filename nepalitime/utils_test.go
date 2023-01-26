62648698903451be4fb427de4a85715529bf41ee
Sugat Bajracharya <sugatbajracharya49@gmail.com>
2023-01-26 20:55:14 +0545

    Unit Test: Add example unit tests

---
+++ nepalitime/utils_test.go
@@ -0,0 +1,52 @@
+package nepalitime
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestTwoDigitNumberWhenNumberIsLessThan0AndGreaterThan0(t *testing.T) {
+	number := 1
+	want := "01"
+
+	got := twoDigitNumber(number)
+
+	assert.Equal(t, want, got, "want and got should be equal")
+}
+
+func TestTwoDigitNumberWhenNumberIsEqualTo10(t *testing.T) {
+	number := 10
+	want := "10"
+
+	got := twoDigitNumber(number)
+
+	assert.Equal(t, want, got, "the value returned should be 10")
+}
+
+func TestTwoDigitNumberGreaterThan10(t *testing.T) {
+	number := 11
+	want := "11"
+
+	got := twoDigitNumber(number)
+
+	assert.Equal(t, want, got, "the value returned should be 11")
+}
+
+func TestTwoDigitNumberEqualTo0(t *testing.T) {
+	number := 0
+	want := "00"
+
+	got := twoDigitNumber(number)
+
+	assert.Equal(t, want, got, "the value returned should be 00")
+}
+
+func TestTwoDigitNumberLessThan0(t *testing.T) {
+	number := -1
+	want := "-1"
+
+	got := twoDigitNumber(number)
+
+	assert.Equal(t, want, got, "the value returned should be -1")
+}
