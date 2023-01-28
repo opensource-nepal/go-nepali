a68912c3bbbd8e1fed15ee9ed72adbb33c991e7a
Ajesh Sen Thapa <aj3sshh@gmail.com>
2023-01-28 23:20:33 +0545

    Update PR review comments, minor unitest code refactoring

--- dateConverter/converter_test.go
+++ dateConverter/converter_test.go
@@ -51,29 +51,20 @@ func TestEnglishToNepaliReturnErrorOnMaxDayRange(t *testing.T) {
 
 func TestEnglishToNepaliReturnsValidPastNepaliDate(t *testing.T) {
 	date, err := dateConverter.EnglishToNepali(1994, 8, 13)
-	assert.NotNil(t, date)
 	assert.Nil(t, err)
-	assert.Equal(t, date[0], 2051)
-	assert.Equal(t, date[1], 4)
-	assert.Equal(t, date[2], 29)
+	assert.EqualValues(t, *date, [3]int{2051, 4, 29})
 }
 
 func TestEnglishToNepaliReturnsValidRecentNepaliDate(t *testing.T) {
 	date, err := dateConverter.EnglishToNepali(2023, 1, 28)
-	assert.NotNil(t, date)
 	assert.Nil(t, err)
-	assert.Equal(t, date[0], 2079)
-	assert.Equal(t, date[1], 10)
-	assert.Equal(t, date[2], 14)
+	assert.EqualValues(t, *date, [3]int{2079, 10, 14})
 }
 
 func TestEnglishToNepaliReturnsValidFutureNepaliDate(t *testing.T) {
 	date, err := dateConverter.EnglishToNepali(2030, 11, 26)
-	assert.NotNil(t, date)
 	assert.Nil(t, err)
-	assert.Equal(t, date[0], 2087)
-	assert.Equal(t, date[1], 8)
-	assert.Equal(t, date[2], 10)
+	assert.EqualValues(t, *date, [3]int{2087, 8, 10})
 }
 
 // NepaliToEnglish
@@ -116,27 +107,18 @@ func TestNepaliToEnglishReturnErrorOnMaxDayRange(t *testing.T) {
 
 func TestNepaliToEnglishReturnsValidPastEnglishDate(t *testing.T) {
 	date, err := dateConverter.NepaliToEnglish(2051, 4, 29)
-	assert.NotNil(t, date)
 	assert.Nil(t, err)
-	assert.Equal(t, date[0], 1994)
-	assert.Equal(t, date[1], 8)
-	assert.Equal(t, date[2], 13)
+	assert.EqualValues(t, *date, [3]int{1994, 8, 13})
 }
 
 func TestNepaliToEnglishReturnsValidRecentEnglishDate(t *testing.T) {
 	date, err := dateConverter.NepaliToEnglish(2079, 10, 14)
-	assert.NotNil(t, date)
 	assert.Nil(t, err)
-	assert.Equal(t, date[0], 2023)
-	assert.Equal(t, date[1], 1)
-	assert.Equal(t, date[2], 28)
+	assert.EqualValues(t, *date, [3]int{2023, 1, 28})
 }
 
 func TestNepaliToEnglishReturnsValidFutureEnglishDate(t *testing.T) {
 	date, err := dateConverter.NepaliToEnglish(2087, 8, 10)
-	assert.NotNil(t, date)
 	assert.Nil(t, err)
-	assert.Equal(t, date[0], 2030)
-	assert.Equal(t, date[1], 11)
-	assert.Equal(t, date[2], 26)
+	assert.EqualValues(t, *date, [3]int{2030, 11, 26})
 }
