99b9b6fe53d433269202d0b7f8786f051751843c
Ajesh Sen Thapa <aj3sshh@gmail.com>
2023-01-29 15:26:17 +0545

    Refactoring and unittests for nepalitime utils

--- nepalitime/utils_test.go
+++ nepalitime/utils_test.go
@@ -1,52 +1,75 @@
-package nepalitime
+package nepalitime_test
 
 import (
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/sugat009/nepali/constants"
+	"github.com/sugat009/nepali/nepalitime"
 )
 
-func TestTwoDigitNumberWhenNumberIsLessThan0AndGreaterThan0(t *testing.T) {
-	number := 1
-	want := "01"
-
-	got := twoDigitNumber(number)
-
-	assert.Equal(t, want, got, "want and got should be equal")
+func TestDateFunctionWithInvalidDateAndTimeArgument(t *testing.T) {
+	nt, err := nepalitime.Date(1, 2, 3, 4, 5, 6, 7)
+	assert.Nil(t, nt)
+	assert.NotNil(t, err)
 }
 
-func TestTwoDigitNumberWhenNumberIsEqualTo10(t *testing.T) {
-	number := 10
-	want := "10"
-
-	got := twoDigitNumber(number)
-
-	assert.Equal(t, want, got, "the value returned should be 10")
+func TestDateFunctionWithValidDateAndTimeArgument(t *testing.T) {
+	nt, err := nepalitime.Date(2079, 10, 15, 14, 29, 6, 7)
+	assert.NotNil(t, nt)
+	assert.Nil(t, err)
 }
 
-func TestTwoDigitNumberGreaterThan10(t *testing.T) {
-	number := 11
-	want := "11"
-
-	got := twoDigitNumber(number)
-
-	assert.Equal(t, want, got, "the value returned should be 11")
+func TestFromEnglishTimeWithUnsupportedDate(t *testing.T) {
+	enTime := time.Date(2083, 1, 29, 14, 29, 6, 7, nepalitime.GetNepaliLocation()) // 2083 is not supported by nepalitime
+	nt, err := nepalitime.FromEnglishTime(enTime)
+	assert.Nil(t, nt)
+	assert.NotNil(t, err)
 }
 
-func TestTwoDigitWhenNumberEqualTo0(t *testing.T) {
-	number := 0
-	want := "00"
-
-	got := twoDigitNumber(number)
-
-	assert.Equal(t, want, got, "the value returned should be 00")
+func TestFromEnglishTimeWithSupportedDate(t *testing.T) {
+	enTime := time.Date(2023, 1, 29, 14, 29, 6, 7, nepalitime.GetNepaliLocation())
+	nt, err := nepalitime.FromEnglishTime(enTime)
+	assert.NotNil(t, nt)
+	assert.Nil(t, err)
 }
 
-func TestTwoDigitWhenNumberLessThan0(t *testing.T) {
-	number := -1
-	want := "-1"
+func TestFromEnglishTimeWithSingaporeTimezone(t *testing.T) {
+	loc, _ := time.LoadLocation("Asia/Singapore")
+	enTime := time.Date(2023, 1, 29, 1, 15, 0, 0, loc) // 2079/10/14 11:00 PM in Nepal
+	nt, _ := nepalitime.FromEnglishTime(enTime)
+	assert.Equal(t, nt.Year(), 2079)
+	assert.Equal(t, nt.Month(), 10)
+	assert.Equal(t, nt.Day(), 14)
+	assert.Equal(t, nt.Hour(), 23)
+	assert.Equal(t, nt.Minute(), 0)
+}
 
-	got := twoDigitNumber(number)
+func TestFromEnglishTimeWithNewYorkTimezone(t *testing.T) {
+	loc, _ := time.LoadLocation("America/New_York")
+	enTime := time.Date(2023, 1, 29, 14, 15, 0, 0, loc) // 2079/10/16 1:00 AM in Nepal
+	nt, _ := nepalitime.FromEnglishTime(enTime)
+	assert.Equal(t, nt.Year(), 2079)
+	assert.Equal(t, nt.Month(), 10)
+	assert.Equal(t, nt.Day(), 16)
+	assert.Equal(t, nt.Hour(), 1)
+	assert.Equal(t, nt.Minute(), 0)
+}
 
-	assert.Equal(t, want, got, "the value returned should be -1")
+func TestNow(t *testing.T) {
+	npTime := nepalitime.Now()
+	assert.NotNil(t, npTime)
+	// TODO: Mocking needed for further check
+}
+
+func TestGetCurrentEnglishTime(t *testing.T) {
+	enTime := nepalitime.GetCurrentEnglishTime()
+	assert.NotNil(t, enTime)
+	// TODO: Mocking needed for further check
+}
+
+func TestGetNepaliLocation(t *testing.T) {
+	loc := nepalitime.GetNepaliLocation()
+	assert.Equal(t, loc.String(), constants.Timezone)
 }
